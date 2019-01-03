b6828727a3822d319952e8f3873fcc4898eaea4a
Dmytro <pankovdm@gmail.com>
2019-01-02 16:48:38 -0800

    range

---
+++ HW07.12.2018range/main.js
@@ -0,0 +1,56 @@
+window.onload = function(){
+
+var range = document.querySelector('#range');
+var input = document.querySelector('#number');
+var greenBlock = document.querySelector('.green-block');
+var redBlock = document.querySelector('.red-block');
+var heightGreen = (document.getElementById('range').valueAsNumber=input.value);
+
+
+function num(){
+    if(input.value>100){
+        return input.value = 100;
+    }
+    range.oninput = function(){
+        return heightGreen=input.value=range.value;
+    }
+    input.oninput = function(){
+        return document.getElementById('range').valueAsNumber=input.value;
+        
+    }
+}
+
+
+
+// function setRange(num,block){
+//     return block.style.height=num+"px";
+// }
+// function setRange(block){
+
+//     document.getElementById('range').valueAsNumber=input.value;
+//     return block.style.height=parseInt(input.value)+"px";
+// }
+
+// function comission(num,block){
+//     if(num==0){return block.style.height=0+"px",
+//     block.style.bottom=num+"px";
+//     }else if(num>0&&num<20){
+//         return block.style.height=2+"px",
+//         block.style.bottom=num+"px";
+//     }else if(num>=20&&num<50){
+//         return block.style.height=4+"px",
+//         block.style.bottom=num+"px";
+//     }else if(num>=50&&num<75){
+//         return block.style.height=6+"px",
+//         block.style.bottom=num+"px";
+//     }else{
+//         return block.style.height=8+"px",
+//         block.style.bottom=num+"px";
+//     }
+// }
+num();
+
+// setRange(heightGreen,greenBlock);
+// comission(heightGreen,redBlock);
+}
+
