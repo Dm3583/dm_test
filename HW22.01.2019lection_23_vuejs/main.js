ab03b867084de4f223c6bd322a0000dbd82432d7
Pankov_Dmytro <pankovdm@gmail.com>
2019-01-28 16:56:56 +0200

    TaskList

--- HW22.01.2019lection_23_vuejs/main.js
+++ HW22.01.2019lection_23_vuejs/main.js
@@ -1,57 +1,67 @@
 var app = new Vue({
     el: '#app',
     data: {
-        list1: [
-                {
-                text: 'Дело1.1'
-                },
-                {
-                text: 'Дело1.2'
-                },
-                {
-                text: 'Дело1.3'
-                },
-                {
-                text: 'Дело1.4'
-                },
-                {
-                text: 'Дело1.5'
-                },
-                {
-                text: 'Дело1.6'
-                }
-                ],
+        lists: [
+                    [
+                    {
+                    text: 'Дело1.1'
+                    },
+                    {
+                    text: 'Дело1.2'
+                    },
+                    {
+                    text: 'Дело1.3'
+                    },
+                    {
+                    text: 'Дело1.4'
+                    },
+                    {
+                    text: 'Дело1.5'
+                    },
+                    {
+                    text: 'Дело1.6'
+                    }
+                    ],
 
-        list2: [
-                {
-                    text: 'Дело2.1'
-                },
-                {
-                    text: 'Дело2.2'
-                },
-                {
-                    text: 'Дело2.3'
-                },
-                {
-                    text: 'Дело2.4'
-                },
-                {
-                    text: 'Дело2.5'
-                },
-                {
-                    text: 'Дело2.6'
-                }
+                    [
+                    {
+                        text: 'Дело2.1'
+                    },
+                    {
+                        text: 'Дело2.2'
+                    },
+                    {
+                        text: 'Дело2.3'
+                    },
+                    {
+                        text: 'Дело2.4'
+                    },
+                    {
+                        text: 'Дело2.5'
+                    },
+                    {
+                        text: 'Дело2.6'
+                    }
+                    ]
                 ]
     },
     methods: {
-        transfer1(list){
-            let lastTask=list[list.length-1];
-            return this.list2.unshift(lastTask),list.pop();
-        },
-        transfer2(list){
-            let lastTask=list[list.length-1];
-            return this.list1.unshift(lastTask),list.pop();
+        transfer(listA,listB){
+            if(listA.length==0||listB.length==0){ console.log(listA); return}
+            if(listA==this.lists[0]){
+                listB = this.lists[1];
+                console.log(listB);
+                let lastTask=listA[listA.length-1];
+                return listB.unshift(lastTask),listA.pop();
+            }
+            if(listA==this.lists[1]){
+                listB = this.lists[0];
+                console.log(listB);
+                let lastTask=listA[listA.length-1];
+                return listB.unshift(lastTask),listA.pop();
+            }
         }
+        
     }
 
   })
