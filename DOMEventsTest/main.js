2e4c5ad214b95d260eb4b8b71e0a2db8b3433c43
Dmytro <pankovdm@gmail.com>
2019-05-24 11:12:35 -0700

    FlagDecision

--- DOMEventsTest/main.js
+++ DOMEventsTest/main.js
@@ -9,14 +9,6 @@ window.onload=function(){
     function move(currPos,n){
         return currPos+n+'px';
     }
-    // function grow(param){
-    //     return param*1.4+'px';
-
-    // }
-
-    // console.log(div);
-    // console.log(div.style.left);
-
 
     document.addEventListener('keypress',
         (event)=>{let keyName = event.key;
@@ -28,11 +20,7 @@ window.onload=function(){
         let curr= parseInt(div.style.height);
         let _left = parseInt(div.style.left);
         let _top = parseInt(div.style.top);
-        // let currW=parseInt(div.style.width);
 
-        // console.log(currW)
-        // console.log(_top);
-            // console.log(typeof div.style.width);
         if(_keyCode===104){
             div.style.height=growH(curr,num);
         }
@@ -51,93 +39,38 @@ window.onload=function(){
         if(_keyCode===100){
             div.style.left=move(_left,num);
         }
-        // if(_keyCode===32){
-        //     div.style.width=currW*1.4+'px';
-        // }
-        // console.log(div.offsetTop)
-
-
-
-    // console.log(_keyCode);
     });
-    // let currTop=parseInt(div.offsetTop);
-    // console.log(currTop);
-    // function cuT(num){
-    //     let num = div.offsetTop;
-    //     return num;
-    // }
+
     let curW=parseInt(div.style.width);
     let curH=parseInt(div.style.height);
 
+    let flag=true;
 
-
-
-
-    function sitDown(event){
+    document.addEventListener('keypress',
+    (event)=>{
         let _keyCode = event.keyCode;
-        
         let currTop=parseInt(div.offsetTop);
         console.log(currTop);
-        switch (_keyCode){
-        case 32:
-        event.repeat=false;
+        if(_keyCode===32&&flag===true){
             div.style.width=curW*1.4+'px';
             div.style.height=curH*0.85+'px';
-
             div.style.top = currTop + (curH*0.15)+'px';
-             
-
-            
             console.log(div.offsetTop);
-            console.log(event.repeat);
-            break;
+            return flag=false;
         }
-
-    }
-    document.addEventListener('keypress', sitDown, false
-    // (event)=>{
-    //     // let currTop=parseInt(div.style.top);
-
-    //     let _keyCode = event.keyCode;
-
-    //     let currTop=parseInt(div.offsetTop);
-    //     console.log(currTop);
-    //     switch (_keyCode){
-    //     case 32:
-            
-    //         div.style.width=curW*1.4+'px';
-    //         div.style.height=curH*0.85+'px';
-
-    //         div.style.top = currTop + (curH*0.15)+'px';
-             
-
-            
-    //         console.log(div.offsetTop);
-    //         break;
-    //     }
-
-        
-
-    // }
-    );
+    });
     console.log(div);
 
-    // let currTop=parseInt(div.offsetTop);
     document.addEventListener('keyup',
     (event)=>{
         let _keyCode = event.keyCode;
-        // let localTop=parseInt(div.offsetTop);
         let localTop=parseInt(div.style.top);
-        if(_keyCode===32){
-            
+        if(_keyCode===32&&flag==false){
             div.style.width=curH+'px';
             div.style.height=curH+'px';
-            // div.style.top=currTopPos()+'px';
             div.style.top=localTop-(curH*0.15)+'px';
-
             console.log(div.offsetTop);
+            return flag=true;
         }
     });
-    // console.log(div.clientTop);
-    // console.log(div.offsetTop);
 };
