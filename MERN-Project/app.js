330fb0a15868188b4f3c27acc94b39a2eadc821a
Pankov_Dmytro <pankovdm@gmail.com>
2020-06-06 21:14:21 +0300

    MERN_router

--- MERN-Project/app.js
+++ MERN-Project/app.js
@@ -6,16 +6,21 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use('/api/auth', require('./routes/auth.routes'));
+
 const PORT = config.get('port') || 5000;
+
 
 
 async function start() {
     try {
 
         await mongoose.connect(config.get('mongoUri'), {
-
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
         })
-
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}....`));
     } catch (e) {
 
         console.log('Server Error', e.message);
@@ -24,6 +29,9 @@ async function start() {
 
     }
 
+
+
 }
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}....`));
+start();
+
